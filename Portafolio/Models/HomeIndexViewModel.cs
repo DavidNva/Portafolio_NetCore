cd52c83776311d938a88367d5d8c50aa6fe5d6cb
David Nava <david.nava.garcia4@gmail.com>
2025-08-17 20:17:06 -0600

    Add _SeccionProyectos.cshtml

---
+++ Portafolio/Models/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Portafolio.Models
+{
+    //Algunos dev le llama por convencion HomeIndexDTO  (Data Transfer Object).
+    //Otros le llamdan HomeIndexViewModel
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Proyecto> Proyectos { get; set; }
+    }
+}
---
+++ Portafolio/Models/Proyecto.cs
@@ -0,0 +1,13 @@
+﻿namespace Portafolio.Models
+{
+    //Algunos dev le llama por convencion ProyectoDTO  (Data Transfer Object).
+    //Otros le llamdan ProyectoViewModel
+    //En este caso lo dejaremos tal cual, teniendo en cuenta que como es Model, sabremos para que es
+    public class Proyecto
+    {
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenURL { get; set; }
+        public string Link { get; set; }
+    }
+}
